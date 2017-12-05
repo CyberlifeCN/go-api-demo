09838e122bc9ff13c552e58e4da90848bea5d805
ThomasZhang <thomas.zh@qq.com>
2017-12-05 20:13:43 +0800

    加入鉴权服务demo

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	_ "go-api-demo/routers"
 
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/plugins/auth"
+	"github.com/casbin/beego-authz/authz"
+	"github.com/casbin/casbin"
 )
 
 
@@ -12,5 +15,11 @@ func main() {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
+
+	// authenticate every request.
+	beego.InsertFilter("*", beego.BeforeRouter, auth.Basic("alice", "123"))
+	// authorize every request.
+	beego.InsertFilter("*", beego.BeforeRouter, authz.NewAuthorizer(casbin.NewEnforcer("./conf/authz_model.conf", "./conf/authz_policy.csv")))
+
 	beego.Run()
 }
